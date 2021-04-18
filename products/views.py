4bbf59b3e100908d8198a95790fb7203603dbff4
J-O-B <Jonathanmichaelobrien@gmail.com>
2021-04-18 23:45:39 +0000

    basic product view and template created

--- products/views.py
+++ products/views.py
@@ -1,2 +1,12 @@
 from django.shortcuts import render
+from .models import Product
 
+
+def all_products(request):
+    products = Product.objects.all()
+
+    template = 'products/products.html'
+    context = {
+        'products': products,
+    }
+    return render(request, template, context)
