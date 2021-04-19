aede09f0bcd4545892c3eccf5bd73384b67b42e7
J-O-B <Jonathanmichaelobrien@gmail.com>
2021-04-19 16:38:21 +0000

    work on product details

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
@@ -8,5 +8,15 @@ def all_products(request):
     template = 'products/products.html'
     context = {
         'products': products,
+    }
+    return render(request, template, context)
+
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+
+    template = 'products/product_detail.html'
+    context = {
+        'product': product,
     }
     return render(request, template, context)
