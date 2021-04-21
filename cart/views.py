8a833029dc1b0752acf4e651b80b3c912b676da1
J-O-B <Jonathanmichaelobrien@gmail.com>
2021-04-21 14:13:55 +0000

    products successfully adding to bag, and being added to cart

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def view_cart(request):
@@ -10,3 +10,18 @@ def view_cart(request):
         'title': title,
     }
     return render(request, template, context)
+
+
+def add_to_cart(request, item_id):
+    """ Add a quantity of the specific product to the cart """
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+    else:
+        cart[item_id] = quantity
+    
+    request.session['cart'] = cart
+    return redirect(redirect_url)
