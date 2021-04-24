33580b08029f762494ba82abd548fd1ac6373e53
J-O-B <Jonathanmichaelobrien@gmail.com>
2021-04-24 13:14:51 +0000

    alignment of icons on homepage

--- cart/views.py
+++ cart/views.py
@@ -18,14 +18,18 @@ def view_cart(request):
 
 def adjust_cart(request, item_id):
     """ Adjust the contents of a users cart items """
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
 
     if quantity > 0:
         cart[item_id] = quantity
+        messages.success(
+            request, f'Updated {product.name} Quantity To {cart[item_id]}')
 
     else:
         cart.pop(item_id)
+        messages.success(request, f'Removed "{product.name}" From Your Cart.')
 
     request.session['cart'] = cart
     return redirect(reverse('cart'))
@@ -40,28 +44,32 @@ def remove_from_cart(request, item_id):
         cart = request.session.get('cart', {})
 
         cart.pop(item_id)
-        messages.success(request, f'Removed {product.name} from your cart.')
+        messages.success(request, f'Removed "{product.name}" From Your Cart.')
 
         request.session['cart'] = cart
         return HttpResponse(status=200)
 
     except Exception as e:
-        messages.error(request, f'Error removing item {e}')
+        messages.error(request, f'Error Removing Item {e}')
         return HttpResponse(status=500)
 
 
 def add_to_cart(request, item_id):
     """ Add a quantity of the specific product to the cart """
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
-    if item_id in list(cart.keys()):
-        cart[item_id] += quantity
-        messages.success(request, f'Added {product.name} to your cart.')
-    else:
-        cart[item_id] = quantity
-
+    try: 
+        if item_id in list(cart.keys()):
+            cart[item_id] += quantity
+            messages.success(
+                request, f'Updated {product.name} Quantity To {cart[item_id]}')
+        else:
+            cart[item_id] = quantity
+            messages.success(request, f'Added {product.name} To Your Cart.')
+    except Exception as e:
+        messages.error(request, f'{e}')
     request.session['cart'] = cart
     return redirect(redirect_url)
