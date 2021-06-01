4409cc7607dbaee8b4917d88c8e3591e3d45b146
J-O-B <Jonathanmichaelobrien@gmail.com>
2021-06-01 15:32:04 +0000

    added email functionality to subscription pages, added placeholder item add to subscription 'sign up' button

--- subscription/views.py
+++ subscription/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import (render, redirect, reverse,
+                              get_object_or_404, HttpResponse)
+from django.contrib import messages
+from django.core.mail import send_mail
+
 
 
 def Subscription(request):
@@ -6,8 +10,54 @@ def Subscription(request):
 
 
 def Gold(request):
+
+    if request.method == "POST":
+        name = request.POST.get("txtName")
+        email = request.POST.get("txtEmail")
+        number = request.POST.get("txtPhone")
+        message = request.POST.get("txtMsg")
+
+        send_mail(
+            f'Gold Subscription: Name: {name}, Tel: {number}',
+            message,
+            email,
+            ['jonathanobrien@outlook.ie'],
+            fail_silently=False,
+        )
+
+        if len(name) > 1:
+            messages.success(request, "Thank you for contacting us.\
+                Your message has been sent and we will respond to \
+                    you as soon as possible. Have a nice day!")
+        else:
+            messages.error(request, "There seems to be an error with your form, \
+                please try again or contact us via email: \
+                    Admin@ManageMyWeb.org")
+
     return render(request, 'subscription/gold.html')
 
 
 def Silver(request):
+    if request.method == "POST":
+        name = request.POST.get("txtName")
+        email = request.POST.get("txtEmail")
+        number = request.POST.get("txtPhone")
+        message = request.POST.get("txtMsg")
+
+        send_mail(
+            f'Silver Subscription: Name: {name}, Tel: {number}',
+            message,
+            email,
+            ['jonathanobrien@outlook.ie'],
+            fail_silently=False,
+        )
+
+        if len(name) > 1:
+            messages.success(request, "Thank you for contacting us.\
+                Your message has been sent and we will respond to \
+                    you as soon as possible. Have a nice day!")
+        else:
+            messages.error(request, "There seems to be an error with your form, \
+                please try again or contact us via email: \
+                    Admin@ManageMyWeb.org")
     return render(request, 'subscription/silver.html')
