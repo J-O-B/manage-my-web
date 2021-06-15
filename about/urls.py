e30e4070811ea3603aa5d9d0a6bda301852b4c2b
J-O-B <Jonathanmichaelobrien@gmail.com>
2021-06-15 17:36:03 +0000

    some homepage styling with margins and text content

--- about/urls.py
+++ about/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.about, name="about"),
+    path('website-management/', views.webManage, name="website-management"),
+    path('seo/', views.seo, name="seo"),
 ]
--- about/views.py
+++ about/views.py
@@ -6,3 +6,11 @@ import json
 
 def about(request):
     return render(request, 'about/about.html')
+
+
+def webManage(request):
+    return render(request, 'about/management.html')
+
+
+def seo(request):
+    return render(request, 'about/seo.html')
