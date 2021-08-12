c5e835b2fc182f9be565056e3796cd0c978da91c
J-O-B <Jonathanmichaelobrien@gmail.com>
2021-08-12 16:58:03 +0000

    added aws bucket, and updated settings files for deployment

--- manage_my_web/settings.py
+++ manage_my_web/settings.py
@@ -45,6 +45,9 @@ INSTALLED_APPS = [
     # Crispy Forms
     'crispy_forms',
 
+    # Storage
+    'storages',
+
     # Natural Time
     'django.contrib.humanize',
 
@@ -187,6 +190,24 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if "USE_AWS" in os.environ:
+    # Bucket Config
+    AWS_STORAGE_BUCKET_NAME = "manage-my-web"
+    AWS_S3_REGION_NAME = "eu-west-1"
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    # Static and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # Override static and media URLs when in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 TAX_RATE = 0.21
 TAX_PERCENTAGE = 21
