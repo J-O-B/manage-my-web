8b3d8a0e6d864266e106db70a0753b58b7602a91
J-O-B <Jonathanmichaelobrien@gmail.com>
2021-08-13 00:54:13 +0000

    complete change of cart page styling, updated to fully responsive design. Reduced information on screen with use of modal

--- manage_my_web/settings.py
+++ manage_my_web/settings.py
@@ -30,7 +30,10 @@ if "SECRET_KEY" in os.environ:
     SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+if 'DEVELOPMENT' in os.environ:
+    DEBUG = False
+else:
+    DEBUG = True
 
 ALLOWED_HOSTS = ['manage-my-web.herokuapp.com', 'localhost']
 
